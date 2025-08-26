6b4d7f30703e4f8125cd1c3f4aa481acc294daf3
aeldab3 <aeldabaa2@gmail.com>
2025-08-26 18:01:59 +0300

    feat(components): use host property that allows us to add properties such as css classes or IDs directly to the host element of the component

--- src/app/dashboard/shared/control/control.component.ts
+++ src/app/dashboard/shared/control/control.component.ts
@@ -6,8 +6,16 @@ import { Component, Input, ViewEncapsulation } from '@angular/core';
   imports: [],
   templateUrl: './control.component.html',
   styleUrl: './control.component.css',
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
+  host: {
+    class: 'control',
+    '(click)': 'onClick()'
+  }
 })
 export class ControlComponent {
   @Input() label!: string;
+
+  onClick() {
+    console.log(`${this.label} clicked`);
+  }
 }
